3fbca04fba0022e6f32faf62bbddb7fc196c4da7
Alex Laurent <shaoner@pm.me>
2022-12-08 17:29:57 -0500

    Add tocbot for side menu

--- public/js/main.js
+++ public/js/main.js
@@ -12,7 +12,7 @@ const saveCurrentTheme = (newTheme) => {
     localStorage.setItem(window.__LS_THEME_KEY, newTheme);
 };
 
-window.onload = function () {
+const setupThemeSwitch = () => {
     const themeSwitchBtn = document.getElementById('theme-switch');
 
     themeSwitchBtn.onchange = () => {
@@ -25,6 +25,23 @@ window.onload = function () {
 
     const theme = getSavedTheme();
     themeSwitchBtn.checked = theme === 'dark';
+};
+
+const setupToc = () => {
+    tocbot.init({
+        tocSelector: '.toc-container',
+        contentSelector: '.content',
+        headingSelector: 'h1, h2, h3, h4, h5',
+        collapseDepth: 1,
+        orderedList: false,
+        scrollSmooth: true,
+        headingsOffset: 200,
+    });
+};
+
+window.onload = function () {
+    setupThemeSwitch();
+    setupToc();
 };
 
 const savedTheme = getSavedTheme();
